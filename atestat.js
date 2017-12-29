85e4b625b3ec2465e197c99e29f40967a53a1bff
preksebe <gameadress599@gmail.com>
2017-12-29 10:40:59 +0200

    smth good

--- atestat.js
+++ atestat.js
@@ -1,8 +1,16 @@
-
+    
+        
         function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
         }
         
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
+        }
+        function openNavv() {
+            document.getElementById("mySidenavv").style.width = "250px";
+        }
+        
+        function closeNavv() {
+            document.getElementById("mySidenavv").style.width = "0";
         }
